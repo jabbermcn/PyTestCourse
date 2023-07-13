e161ac9a3d09b9fb3fa15fcba0791f7bb841eecc
mikhailouski_n <jabber_mcn@tut.by>
2023-07-13 10:17:53 +0300

    Initial commit

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+import pytest
+from src.main import Calculator
+
+
+@pytest.mark.parametrize(
+    'x, y, res',
+    [
+        (1, 2, 0.5),
+        (5, -1, -5)
+    ]
+)
+def test_divide(x, y, res):
+    assert Calculator().divide(x, y) == res
